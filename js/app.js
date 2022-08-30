602be9757168713deb44232d48d55b13f93415c5
opuzayed <zayedopu4933@gmail.com>
2022-08-30 16:06:45 +0600

    load phone and display phone

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const loadPhones = async() =>
+{
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+const displayPhones = phones => {
+    console.log(phones);
+}
+loadPhones();
