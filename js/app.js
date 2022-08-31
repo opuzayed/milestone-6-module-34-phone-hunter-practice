d2679ba48cfb229cc960ba86e82a3bb2487032ee
opuzayed <zayedopu4933@gmail.com>
2022-08-31 16:41:40 +0600

    show button show details information into modals and also use ternary operator

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ const displayPhones = (phones, dataLimit) => {
                         <div class="card-body">
                           <h5 class="card-title">${phone.phone_name}</h5>
                           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                          <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
+                          <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
                         </div>
                       </div>
     `;
@@ -85,8 +85,20 @@ const loadPhoneDetails = async id => {
   const url =  `https://openapi.programming-hero.com/api/phone/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.data);
+  displayPhoneDetails(data.data);
 }
 
+//-----------display-phone-details---------
+const displayPhoneDetails = phone =>
+{
+  console.log(phone);
+  const modalTitle = document.getElementById('phoneDetailModalLabel');
+  modalTitle.innerText = phone.name;
+  const phoneDetails = document.getElementById('phone-details');
+  phoneDetails.innerHTML = `
+  <p>Release Date : ${phone.releaseDate ? phone.releaseDate : 'no release date found'}</p>
+  <p>Others : ${phone.others ? phone.others.Bluetooth : 'no bluetooth found'}</p>
+  `;
+}
 
-//loadPhones();
+loadPhones('oppo');
